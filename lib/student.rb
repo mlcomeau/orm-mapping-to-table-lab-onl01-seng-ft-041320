3e0f5611582a16c2addd728e9f2d665cc899a66d
Megan Comeau <mlcomeau99@gmail.com>
2020-05-14 19:43:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,24 +1,24 @@
 class Student
   attr_accessor :name, :grade
-  attr_reader :id 
+  attr_reader :id
 
   def initialize(name, grade, id=nil)
-    @id = id 
-    @name = name 
-    @grade = grade 
-  end 
+    @id = id
+    @name = name
+    @grade = grade
+  end
 
   def self.create_table
-    sql = <<-SQL 
+    sql = <<-SQL
     CREATE TABLE IF NOT EXISTS songs (
-      id INTEGER PRIMARY KEY, 
-      name TEXT, 
+      id INTEGER PRIMARY KEY,
+      name TEXT,
       grade INTEGER
     )
     SQL
     DB[:conn].execute(sql)
-  end 
+  end
 
-  
+
 
 end
