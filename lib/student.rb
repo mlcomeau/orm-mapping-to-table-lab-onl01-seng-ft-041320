3ca2dfc887a69f0c7d1e25de9a7a9f4be1382bf9
Megan Comeau <mlcomeau99@gmail.com>
2020-05-14 19:52:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,9 +38,9 @@ class Student
 
   def self.create (name, grade)
     student = Student.new(name, grade)
-    student.save 
-    student 
-  end 
-    
+    student.save
+    student
+  end
+
 
 end
